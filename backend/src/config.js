2d535f5844a3abbe7cdfcd242a6b69e921d9e19a
beatrixblome <beatrixyublome@gmail.com>
2022-04-21 00:49:55 +0200

    change mint price

--- backend/src/config.js
+++ backend/src/config.js
@@ -70,7 +70,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0xCcC121Ef0d2b6b749a35ad11ccd62feFbcbda503';
 const TREASURY_ADDRESS = '0xCcC121Ef0d2b6b749a35ad11ccd62feFbcbda503'; // if you want to withdraw the transaction
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.0001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
