eb54e10ab139bf787f74773bc2779a3d14b87e2b
beatrixblome <beatrixyublome@gmail.com>
2022-04-23 15:54:50 +0200

    whitelist updated

--- backend/src/config.js
+++ backend/src/config.js
@@ -77,7 +77,7 @@ const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single
 const PUBLIC_MINT_START_DATE = "2022-05-21T12:00:00+02:00"; // This is UTC timezone. This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-04-23T12:00:00+02:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-04-22T12:00:00+02:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xCcC121Ef0d2b6b749a35ad11ccd62feFbcbda503"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri. After the NFT is minted
