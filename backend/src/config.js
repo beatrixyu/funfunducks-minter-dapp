3add951a83e2ab0586efa8100c4cc712518a58e7
beatrixblome <beatrixyublome@gmail.com>
2022-04-23 16:54:11 +0200

    public

--- backend/src/config.js
+++ backend/src/config.js
@@ -74,7 +74,7 @@ const MINT_PRICE = 0.000001; // Minting price per NFT. Rinkeby = ETH, Polygon = 
 const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-05-21T12:00:00+02:00"; // This is UTC timezone. This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-04-23T12:00:00+02:00"; // This is UTC timezone. This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-04-23T12:00:00+02:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
